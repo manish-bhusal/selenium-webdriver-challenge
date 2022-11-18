b7107dedd79358ade03052a26f387aa1226884a4
manish-bhusal <manishbhusal6@gmail.com>
2022-11-18 11:59:26 +0545

    Complete Mouse Actions

--- interaction.py
+++ interaction.py
@@ -12,9 +12,10 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=chrome_driver_path, options=chrome_options)
 driver.maximize_window()
 # ----------Keyboard Actions Start------------
-URL = "https://www.facebook.com"
+# URL = "https://www.facebook.com"
+URL = "https://selenium.dev/selenium/web/mouse_interaction.html"
 driver.get(URL)
-driver.find_element(By.ID, "email").send_keys("manish@gmail.com")
+# driver.find_element(By.ID, "email").send_keys("manish@gmail.com")
 
 act = ActionChains(driver)
 # Long Form
@@ -35,5 +36,29 @@ act = ActionChains(driver)
 # ------------Keyboard Actions End----------------
 
 # -----------Copy Paste--------------
-act.key_down(Keys.CONTROL).key_down(
-    Keys.SHIFT).send_keys(Keys.ARROW_LEFT).send_keys(Keys.ARROW_LEFT).perform()
+# act.key_down(Keys.CONTROL).key_down(
+#     Keys.SHIFT).send_keys(Keys.ARROW_LEFT).send_keys(Keys.ARROW_LEFT).perform()
+
+# ---------Mouse Actions Start---------------
+# clickable = driver.find_element(By.ID, "clickable")
+# clickable = driver.find_element(By.ID, "click")
+#Click and Hold
+# act.click_and_hold(clickable).perform()
+# Click(Left Click)
+# act.click(clickable).perform()
+# Right Click
+# act.context_click(clickable).perform()
+
+# Double Click
+# act.double_click(clickable).perform()
+
+# Hovering
+# hoverable = driver.find_element(By.ID, "hover")
+# act.move_to_element(hoverable).perform()
+
+# Drag and Drop
+draggable = driver.find_element(By.ID, "draggable")
+droppable = driver.find_element(By.ID, "droppable")
+act.drag_and_drop(draggable, droppable).perform()
+
+# -----------Mouse Actions End----------
